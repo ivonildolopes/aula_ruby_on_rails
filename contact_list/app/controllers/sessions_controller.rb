0c1a76210a9633a7e40b9ab0b2d4bfbd1efdd6b4
Ivonildo Rodrigues <ivonildolopes.ti@gmail.com>
2019-01-30 13:49:35 -0300

    estilizando o form new do contato

--- contact_list/app/controllers/sessions_controller.rb
+++ contact_list/app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
         if user && user.authenticate(params[:session][:password])
             sign_in(user);
-            redirect_to user_path(user);
+            redirect_to contacts_path
         else
             flash.now[:danger] = 'Email ou senha invalidos';   
             render 'new';  
