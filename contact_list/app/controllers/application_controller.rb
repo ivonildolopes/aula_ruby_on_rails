9d2b6ed0fb2978002ec952dc4f00d5ae441a5ab4
Ivonildo Rodrigues <ivonildolopes.ti@gmail.com>
2019-01-30 12:36:23 -0300

    restrigindo rotas para somente logado

--- contact_list/app/controllers/application_controller.rb
+++ contact_list/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
 
     include SessionsHelper
+
+    private 
+        def require_logged_in_user
+            flash[:danger] = 'Area restrita, por favor realize o Login'
+            redirect_to login_path
+        end
+
 end
