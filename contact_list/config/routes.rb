332cb56aab692000ef6e9e471d73bca86f48b7c8
Ivonildo Rodrigues <ivonildolopes.ti@gmail.com>
2019-01-30 12:17:25 -0300

    primeira parte login

--- contact_list/config/routes.rb
+++ contact_list/config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "static_pages#index";
+
   get 'sobre', to: "static_pages#sobre";
   get 'contato', to: "static_pages#contato";
+  get 'login', to: "sessions#new";
+  post 'login', to: "sessions#create";
+  delete 'sair', to: "sessions#destroy";
   
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
 end
