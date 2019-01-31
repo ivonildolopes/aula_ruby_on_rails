983e0069f6d822e3f3fcbbd5357090e5b6eee660
Ivonildo Rodrigues <ivonildolopes.ti@gmail.com>
2019-01-31 10:26:30 -0300

    criando novo projeto

--- pwa/db/seeds.rb
+++ pwa/db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create([{name: 'João Rodrigues', email: 'joao@gmail.com', password_digest: '123'}])
